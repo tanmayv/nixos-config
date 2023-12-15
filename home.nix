c55bdefea2b1d3d13107ecf5e1b47f8beb6b9edf
Samuel Hale <samworlds1231337@gmaiil.com>
2023-12-14 20:19:31 -0500

    fixed cursor and theme

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
  
   home.username = "samuel";
   home.homeDirectory = "/home/samuel";
-
+  
 
   # basic configuration of git, please change to your own
   programs.git = {
@@ -152,13 +152,8 @@
     };
 
     theme = {
-      name = "Whitesur-Dark";
+      name = "WhiteSur-Dark";
       package = pkgs.whitesur-gtk-theme;
-    };
-
-    cursorTheme = {
-      name = "MacOS-BigSur";
-      package = pkgs.apple-cursor;
     };
 
     gtk3.extraConfig = {
@@ -174,8 +169,31 @@
     };
   };
 
-  home.sessionVariables.GTK_THEME = "Whitesur-Dark";
+  home.sessionVariables.GTK_THEME = "WhiteSur-Dark";
+  
 
+  home.pointerCursor = 
+    let 
+      getFrom = url: hash: name: {
+          gtk.enable = true;
+          x11.enable = true;
+          name = name;
+          size = 26;
+          package = 
+            pkgs.runCommand "moveUp" {} ''
+              mkdir -p $out/share/icons
+              ln -s ${pkgs.fetchzip {
+                url = url;
+                hash = hash;
+              }} $out/share/icons/${name}
+          '';
+        };
+    in
+      getFrom 
+        "https://github.com/ful1e5/apple_cursor/releases/download/v2.0.0/macOS-Monterey.tar.gz"
+        "sha256-MHmaZs56Q1NbjkecvfcG1zAW85BCZDn5kXmxqVzPc7M="
+        "macOS-Monterey";
+  
 
 
 
